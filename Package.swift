d8343acf10effc16885a3cd5fc120213281638ee
Anthony Todd <anthony.todd@xibis.com>
2021-09-30 10:58:40 +0100

    CHORE: UPDATED PLATFORMS TO FIX GRDB DEPENDENCY

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "RxGRDB",
     platforms: [
-        .iOS("10.0"),
-        .macOS("10.10"),
-        .tvOS("9.0"),
-        .watchOS("2.0"),
+        .iOS(.v11),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2),
     ],
     products: [
         .library(name: "RxGRDB", targets: ["RxGRDB"]),
